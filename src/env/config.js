a4dfcd3bce36a2c2596076360472eb4be27b4f14
smartluobo <1043254182@qq.com>
2020-05-28 01:15:01 +0800

    console.log报错修改

--- src/env/config.js
+++ src/env/config.js
@@ -1,6 +1,6 @@
 var obj = {
   devUrl: 'http://47.106.172.126:8888/',
-  onlineUrl: 'http://47.106.172.126:9001/',
+  onlineUrl: 'http://47.106.172.126:8888/',
   isDevEnv: false,
 }
 export default obj
